01544b09679756d18ee6dbd33e23eb9db6afef4f
dbo1093 <dbo1093@gmail.com>
2018-12-22 22:09:45 +0200

    map gets current locaiton

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,23 +1,77 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, Dimensions } from 'react-native';
 import { Button, Card, CardSection, Input, Spinner } from './common';
 import MapView from 'react-native-maps';
 
-class MapContainer extends Component {
-    state = { email: '', password: '', error: '', loading: false };
+const {width, height} = Dimensions.get('window');
 
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
+class MapContainer extends Component {
+    state = { 
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      }, 
+      markerPosition: {
+        latitude: 0,
+        longitude: 0
+      }
+    };
+
+
+    watchID: ?number = null;
+
+    componentDidMount() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = parseFloat(position.coords.latitude);
+        const lon = parseFloat(position.coords.longitude);
+
+        const initialRegion = {
+          latitude: lat,
+          longitude: lon,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+
+        this.setState({initialPosition: initialRegion});
+        this.setState({markerPosition: initialRegion});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+      this.watchID = navigator.geolocation.watchPosition((position) => {
+        const lat = parseFloat(position.coords.latitude);
+        const lon = parseFloat(position.coords.longitude);
+
+        const lastRegion = {
+          latitude: lat,
+          longitude: lon,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+
+        this.setState({initialPosition: lastRegion});
+        this.setState({markerPosition: lastRegion});
+      })
+    }
+
+    componentWillUnmount() {
+      navigator.geolocation.clearWatch(this.watchID);
+    }
 
     render() {
         return (
             <Card style={styles.container}>
                 <MapView
                   style={styles.map}
-                  region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                  }}
+                  region={this.state.initialPosition}
                 />
             </Card>
         );
